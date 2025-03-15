225270dd401b7700b8638df526610bf396b8b649
KrishnaNair <nairkrishnauk@gmail.com>
2025-03-15 01:52:39 +0000

    Added Comments To l2_e2 Final

--- v2.9.0-v2.7.0/l2/l2_e2/src/main.c
+++ v2.9.0-v2.7.0/l2/l2_e2/src/main.c
@@ -11,14 +11,32 @@
 #include <dk_buttons_and_leds.h>
 
 /* STEP 2.1 - Declare the Company identifier (Company ID) */
+#define COMPANY_ID_CODE            0x0059 /* Company ID for Nordic Semiconductor ASA */
 
 /* STEP 2.2 - Declare the structure for your custom data  */
+typedef struct adv_mfg_data {
+	uint16_t company_code;	    /* Company Identifier Code. */
+	uint16_t number_press;      /* Number of times Button 1 is pressed*/	
+} adv_mfg_data_type;
+
 
 #define USER_BUTTON DK_BTN1_MSK
 
-/* STEP 1 - Create an LE Advertising Parameters variable */
+/* STEP 1 - Create an LE Advertising Parameters variable 
+    step increase = 0.625ms
+	Min delay = 20ms = 20/0.625 = 32 N
+	Max delay = 10.24s = 1024/0.625 = 16384 N
+	Advertising interval is set to 500 ms -> 500ms/0.625ms = 800 N
+	As we discussed before, there will be a random delay added to avoid packet collisions.
+************************************************************************************************/
+static const struct bt_le_adv_param *adv_param =
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE,
+	800,
+	801,
+	NULL);
 
 /* STEP 2.3 - Define and initialize a variable of type adv_mfg_data_type */
+static adv_mfg_data_type adv_mfg_data = {COMPANY_ID_CODE,0x00};
 
 static unsigned char url_data[] = { 0x17, '/', '/', 'a', 'c', 'a', 'd', 'e', 'm',
 				    'y',  '.', 'n', 'o', 'r', 'd', 'i', 'c', 's',
@@ -33,8 +51,9 @@ LOG_MODULE_REGISTER(Lesson2_Exercise2, LOG_LEVEL_INF);
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),	
 	/* STEP 3 - Include the Manufacturer Specific Data in the advertising packet. */
+	BT_DATA(BT_DATA_MANUFACTURER_DATA,(unsigned char *)&adv_mfg_data, sizeof(adv_mfg_data)),
 
 };
 
@@ -42,8 +61,27 @@ static const struct bt_data sd[] = {
 	BT_DATA(BT_DATA_URI, url_data, sizeof(url_data)),
 };
 /* STEP 5 - Add the definition of callback function and update the advertising data dynamically */
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & button_state & USER_BUTTON) {
+	adv_mfg_data.number_press += 1;	
+	bt_le_adv_update_data(ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	}
+}
 
 /* STEP 4.1 - Define the initialization function of the buttons and setup interrupt.  */
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+
+	return err;
+}
 
 int main(void)
 {
@@ -58,6 +96,12 @@ int main(void)
 		return -1;
 	}
 	/* STEP 4.2 - Setup buttons on your board  */
+	err = init_button();
+	if (err) {
+
+		printk("Button init failed (err %d)\n", err);
+		return -1;
+	}
 
 	err = bt_enable(NULL);
 	if (err) {
@@ -75,8 +119,8 @@ int main(void)
 
 	LOG_INF("Advertising successfully started\n");
 
-	for (;;) {
+	for (;;) {		
 		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
-		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));		
 	}
 }
